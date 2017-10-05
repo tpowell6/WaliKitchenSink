b278e07713922e336221fdc05d87602dd0cb815d
tpowell6 <walipowell@yahoo.com>
2017-10-04 18:49:38 -0700

    Server combined with nodemailer

--- WifiClient2/Server/Server.js
+++ WifiClient2/Server/Server.js
@@ -7,6 +7,30 @@ var methodOverride = require('method-override');
 var hostname = process.env.HOSTNAME || 'localhost';
 var port = 8080;
 
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'wtmp20@gmail.com',
+    pass: 'walipowe20'
+  }
+});
+
+const mailOptions = {
+  from: 'wtmp20@gmail.com',
+  to: 'wpowell321@gmail.com',
+  subject: 'Sending Email using Node.js',
+  html: '<p>That was easy!<p>'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(info);
+  }
+});
+
 app.get("/", function (req, res) {
     console.log("GET req arrived")
         res.send("hello world")
